c97c16c20d707c7b1b6dc32fa585ccb79f3c4672
vivizuma <vito.manzullo@live.co.uk>
2025-02-19 02:28:06 +0000

    linked ejs view to a route

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const path = require("node:path")
 // set view engine to ejs
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
+
+
 app.get("/", (req,res)=>res.send("hello World"))
-app.get("/index", (req,res)=> res.render("ejs"))
+app.get("/index", (req,res)=> res.render("index", {message: "hello"}))
 // new message route
 app.get("/", (req,res)=> {res.send("new message form"); })
 
